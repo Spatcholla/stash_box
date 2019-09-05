2699fadf466bbe9d84b1d6974edc6bff3a4722e4
Samuel Moore <samuel@odinware.com>
2019-09-05 02:20:22 -0500

    Add bytes human readable converter python functions

---
+++ size_converter.py
@@ -0,0 +1,14 @@
+def sizeof_fmt_base2(num, suffix='B'):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f} {unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f} Yi{suffix}"
+
+
+def sizeof_fmt_base10(num, suffix='B'):
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1000.0:
+            return f"{num:3.1f} {unit}{suffix}"
+        num /= 1000.0
+    return f"{num:.1f} Y{suffix}"
